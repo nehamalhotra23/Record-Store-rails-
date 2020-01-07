ad01f783806d8840dd736bc94a1f9a2622d22f6e
Neha Malhotra <nehamalhotra8899@gmail.com>
2020-01-07 11:46:22 -0800

    add controller and views

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'albums#index'
+  resources :albums do
+    resources :songs
+  end
+end
+
+# if we don't want user to destroy songs we might do this :
+#   resources :songs, except: [:destroy]
+
+
+# If we wanted users only to be able to see a list of Songs but not be able to do any other CRUD functionality, we could do this:
+#
+#   resources :songs, only: [:index]
+#
+#   We can also do more complex routing as well, including creating non-resourceful routes, redirects, and customizing just about every part of a route. Check out the: https://guides.rubyonrails.org/routing.html
