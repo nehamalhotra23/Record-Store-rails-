5777488f992106583c810f763eee4727c6e1d928
Neha Malhotra <nehamalhotra8899@gmail.com>
2020-01-08 13:45:18 -0800

    add faker gem to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,21 @@
-album_list = [
-  [ "Oak", "Quercus" ],
-  [ "Pine", "Pinus" ],
-  [ "Sycamore", "Platanus" ],
-  [ "Alder", "Alnus" ],
-  [ "Birch", "Betula"],
-  [ "Cherry", "Prunus"]
-]
-
-album_list.each do |album, song|
-  Album.create( name: album, name: song )
+Album.destroy_all
+50.times do |index|
+  Album.create!(name: Faker::Music.album, year: Faker::Number.between(from: 1900, to: 2019))
 end
+
+p "Created #{Album.count} albums"
+
+
+
+# album_list = [
+#   [ "Oak", "Quercus" ],
+#   [ "Pine", "Pinus" ],
+#   [ "Sycamore", "Platanus" ],
+#   [ "Alder", "Alnus" ],
+#   [ "Birch", "Betula"],
+#   [ "Cherry", "Prunus"]
+# ]
+#
+# album_list.each do |album, song|
+#   Album.create( name: album, name: song )
+# end
