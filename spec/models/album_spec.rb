239c3a3ae6adb2944afb56853c50bf5c3cf74f20
Neha Malhotra <nehamalhotra8899@gmail.com>
2020-01-08 11:50:55 -0800

    add comments in test file

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -4,7 +4,8 @@ describe Album do
 it { should have_many(:songs) }
 it { should validate_presence_of :name }
 it { should validate_length_of(:name).is_at_most(100)}
-# it {should have_and_belong_to_many :artists}
+# it {should have_and_belong_to_many :artists} this test suppose to fails AFTER adding has_many_through
+
 it { should have_many(:artists).through(:album_artists) }
 
 it('titleizes the name of the album') do
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Artist do
-  # it { should have_and_belong_to_many :albums}
+  # it { should have_and_belong_to_many :albums} this test suppose to fails AFTER adding has_many_through
 
   it { should have_many(:albums).through(:album_artists) }
 end
